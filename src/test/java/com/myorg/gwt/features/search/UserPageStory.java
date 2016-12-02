e5ad9e10c01b666e77c663566441b4d4ed19afe5
Zhigalov-E <zhigalov_java@mail.ru>
2016-12-02 09:45:46 +0200

    Add dataset setUp and tearDown

--- src/test/java/com/myorg/gwt/features/search/UserPageStory.java
+++ src/test/java/com/myorg/gwt/features/search/UserPageStory.java
@@ -1,12 +1,19 @@
 package com.myorg.gwt.features.search;
 
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import com.myorg.gwt.entities.UserRow;
 import com.myorg.gwt.steps.serenity.LoginSteps;
 import com.myorg.gwt.steps.serenity.UserListSteps;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
+import net.thucydides.junit.spring.SpringIntegration;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -15,22 +22,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RunWith(SerenityRunner.class)
+@ContextConfiguration("classpath:spring-data-context.xml")
+@TestExecutionListeners({DbUnitTestExecutionListener.class})
 public class UserPageStory extends BaseSerenityStory  {
 
     public static final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+
+    @Rule
+    public SpringIntegration springIntegration = new SpringIntegration();
 
     @Steps
     private UserListSteps userListSteps;
     @Steps
     private LoginSteps loginSteps;
 
+
     @Test
+    @DatabaseSetup("/data/setupUsersData.xml")
+    @DatabaseTearDown("/data/tearDownUsersData.xml")
     public void checkUsersList() throws ParseException {
-        loginSteps.login(LoginStory.LOGIN, LoginStory.PASSWORD);
+        loginSteps.login("john3", "secret");
         userListSteps.goToUsersList();
         List<UserRow> actualRows = new ArrayList<>();
-        actualRows.add(new UserRow("John", "Looters",formatter.parse("1976-02-05"),"looters.john@gmail.com"));
-        actualRows.add(new UserRow("Иван","Петров",formatter.parse("1986-02-05"),"petrov.ivan@gmail.com"));
+        actualRows.add(new UserRow("John3", "Smith",formatter.parse("1985-02-05"),"john3.smith3@gmail.com"));
+        actualRows.add(new UserRow("John", "Smith",formatter.parse("1986-02-05"),"john.smith@gmail.com"));
+        actualRows.add(new UserRow("John5", "Smith",formatter.parse("1986-02-05"),"john5.smith5@gmail.com"));
+        actualRows.add(new UserRow("John2", "Smith",formatter.parse("1987-02-05"),"john2.smith2@gmail.com"));
+        actualRows.add(new UserRow("John4", "Smith",formatter.parse("1988-02-05"),"john4.smith4@gmail.com"));
         userListSteps.checkTableRows(actualRows);
     }
 }
